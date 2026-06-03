55f8d0336a5542ab68d2677b4042e98d8b99404d
fanyouwu555 <fanyouwu555@gmail.com>
2026-06-03 22:28:31 +0800

    feat(common): 添加JSONB类型处理器
    - 支持PostgreSQL JSONB类型的字段映射

---
+++ aeisp-common/src/main/java/com/aeisp/common/typehandler/JsonbTypeHandler.java
@@ -0,0 +1,67 @@
+package com.aeisp.common.typehandler;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.type.MappedJdbcTypes;
+import org.apache.ibatis.type.MappedTypes;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Map;
+
+/**
+ * PostgreSQL JSONB 类型处理器
+ *
+ * @author AEISP Team
+ */
+@MappedTypes({Object.class, Map.class})
+@MappedJdbcTypes(JdbcType.OTHER)
+public class JsonbTypeHandler extends BaseTypeHandler<Object> {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private final Class<?> type;
+
+    public JsonbTypeHandler(Class<?> type) {
+        this.type = type;
+    }
+
+    @Override
+    public void setNonNullParameter(PreparedStatement ps, int i, Object parameter, JdbcType jdbcType) throws SQLException {
+        try {
+            String json = objectMapper.writeValueAsString(parameter);
+            ps.setString(i, json);
+        } catch (JsonProcessingException e) {
+            throw new SQLException("Error converting object to JSON", e);
+        }
+    }
+
+    @Override
+    public Object getNullableResult(ResultSet rs, String columnName) throws SQLException {
+        return parseJson(rs.getString(columnName));
+    }
+
+    @Override
+    public Object getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+        return parseJson(rs.getString(columnIndex));
+    }
+
+    @Override
+    public Object getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+        return parseJson(cs.getString(columnIndex));
+    }
+
+    private Object parseJson(String json) throws SQLException {
+        if (json == null) {
+            return null;
+        }
+        try {
+            return objectMapper.readValue(json, type);
+        } catch (JsonProcessingException e) {
+            throw new SQLException("Error parsing JSON", e);
+        }
+    }
+}
